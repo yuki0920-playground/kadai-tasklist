b62ec3155abfdd04f8fbeec04b138ccdb2597e6f
yw0920 <ywsep20@gmail.com>
2019-03-17 03:30:05 +0000

    CRUDの完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,8 +10,38 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが投稿されませんでした'
+      render :new
+    end
+  end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    
+    @task.destroy
+    redirect_to tasks_url
   end
   
   private
