20edc0d83bf9277082886948765e5bf109497fc6
yw0920 <ywsep20@gmail.com>
2019-03-23 03:43:06 +0000

    session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # get 'users/index'
   # get 'users/show'
   # get 'users/new'
@@ -9,6 +12,10 @@ Rails.application.routes.draw do
   # get 'tasks/edit'
   
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
