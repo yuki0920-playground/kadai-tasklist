311308975e11ec786d3be37515758fe33d4e3c14
yw0920 <ywsep20@gmail.com>
2019-03-23 02:54:48 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/create'
   # get 'tasks/index'
   # get 'tasks/show'
   # get 'tasks/new'
   # get 'tasks/edit'
   
   root to: 'tasks#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   resources :tasks
   
 end
